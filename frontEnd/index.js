ae0bc822974b19c780ae5dbaec2110836e1c6b4d
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2022-11-25 18:04:17 +0530

    checking out frontend

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,28 +1,28 @@
 function userlogin() {
- // const emailId  = document.getElementById("username").value;
+  // const emailId  = document.getElementById("username").value;
   //const password = document.getElementById("password").value;
   const returnvalue = document.getElementById("response");
   //id1.innerHTML = emailId.value+password.value
   //id1.addEventListener('click',userlogin);
 
   var valis = CheckValidity();
-  //returnvalue.innerHTML = valis[0]
-  returnvalue.addEventListener('click',CheckValidity);
-    return valis[0];
+  returnvalue.innerHTML = valis;
+  returnvalue.addEventListener("click", CheckValidity);
+  return valis[0];
 }
 
 async function CheckValidity() {
-    //const endpoint = new URL(`http://localhost:5050/instructor-login/${username}/${password}`);
-    // const endpoint = new URL ('http://localhost:5050/retrieve-instructors')
-  let response = await fetch('http://localhost:5050/retrieve-instructors');
+  //const endpoint = new URL(`http://localhost:5050/instructor-login/${username}/${password}`);
+  // const endpoint = new URL ('http://localhost:5050/retrieve-instructors')
+  let response = await fetch("http://localhost:5050/retrieve-instructors");
   let json;
-  if (response.ok === true){
-      json = await response.json();
-     console.log(json[0])
-  }else{
-    console.log("iam here")
+  if (response.ok === true) {
+    json = await response.json();
+    console.log(json[0]);
+  } else {
+    console.log("iam here");
     alert("HTTP-Error: " + response.status);
   }
-
-  return json;
+  let raw = JSON.stringify(json,undefined,2);
+  return raw;
 }
