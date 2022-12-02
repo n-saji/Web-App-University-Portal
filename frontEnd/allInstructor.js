aecc57ae3de40c829189999a89bd8596f9f22f74
Nikhil Saji <103165218+Nikhil-Wiz@users.noreply.github.com>
2022-12-03 00:14:10 +0530

    Feature branch (#26)
    * made pipline for main and master
    
    * Made response msg meaningfull for delete
    
    * ran go build
    
    * renamed few files
    
    * changed endpoint names
    
    * made string in lower case
    
    * missing api made
    
    * refactored
    
    * redefined errors
    
    * minor changes
    
    * Added GRPC - not implemented
    
    * Making front end
    
    * checking out frontend
    
    * :wrench: giff
    
    * added basic CSS,HTML,JS instructor
    
    * added page for inserting user
    
    * bug fix

---
+++ frontEnd/allInstructor.js
@@ -0,0 +1,23 @@
+async function populateInstructors() {
+  let all_instructors = await fetch(
+    `http://localhost:5050/retrieve-instructors`
+  );
+  let all_instructors_response = await all_instructors.json();
+
+  document.getElementById("instructorTable").innerHTML =
+    all_instructors_response
+      .map(
+        (user) =>
+          `<tr>
+    <td>${user.InstructorCode}</td>
+    <td>${user.InstructorName}</td>
+    <td>${user.Department}</td>
+    <td>${user.CourseName}</td>
+    </tr>`
+      )
+      .join("");
+}
+
+async function insertInstructor() {
+  window.open("createInstructor.html");
+}
