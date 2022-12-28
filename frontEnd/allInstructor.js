7586d040e631354d4bc5086b38ad3b015f71ab14
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2022-12-29 01:19:43 +0530

    added functionality

--- frontEnd/allInstructor.js
+++ frontEnd/allInstructor.js
@@ -1,37 +1,14 @@
 async function populateInstructors() {
-  let all_instructors = await fetch(
-    `http://localhost:5050/retrieve-instructors`
-  );
-  let all_instructors_response = await all_instructors.json();
-  document.getElementById("instructor_table").style.display = "table";
-  document.getElementById("instructor_table_head").innerHTML = `
-  <tr>
-    <th>${"InstructorCode"}</th>
-    <th>${"InstructorName"}</th>
-    <th>${"Department"}</th>
-    <th>${"CourseName"}</th>
-  </tr>`;
-  document.getElementById("instructor_table_body").innerHTML =
-    all_instructors_response
-      .map(
-        (user) =>
-          `<tr>
-    <td>${user.InstructorCode}</td>
-    <td>${user.InstructorName}</td>
-    <td>${user.Department}</td>
-    <td>${user.CourseName}</td>
-    </tr>`
-      )
-      .join("");
+  window.location.replace("instructorDetails.html");
 }
 
 async function insertInstructor() {
   window.location.replace("createInstructor.html");
 }
 
-function hideInstructors() {
-  document.getElementById("instructor_table").style.display = "none";
-}
+// function hideInstructors() {
+//   document.getElementById("instructor_table").style.display = "none";
+// }
 function setbackpage() {
   window.location.replace("index.html");
 }
---
+++ frontEnd/instructorDetails.js
@@ -0,0 +1,23 @@
+async function populateInstructors() {
+  let all_instructors = await fetch(
+    `http://localhost:5050/retrieve-instructors`
+  );
+  let all_instructors_response = await all_instructors.json();
+  for (let i = 0; i < all_instructors_response.length; i++) {
+    let each_value = all_instructors_response[i];
+    let table1 = document.getElementById("instructor_table");
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td>${each_value.InstructorCode}</td>
+       <td>${each_value.InstructorName}</td>
+       <td>${each_value.Department}</td>
+       <td>${each_value.CourseName}</td>`;
+    table1.appendChild(tr);
+  }
+}
+populateInstructors();
+function setdashboard() {
+  window.location.replace("allinstructor.html");
+}
+function setbackpage() {
+  window.location.replace("index.html");
+}
