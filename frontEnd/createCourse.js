4ef216edba229bc4be8bea790cbabbfacd40558e
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2022-12-29 12:52:28 +0530

    Created function for course

--- frontEnd/createCourse.js
+++ frontEnd/createCourse.js
@@ -5,3 +5,28 @@ function setbackpage() {
 function setdashboard() {
   window.location.replace("allinstructor.html");
 }
+
+async function InsertCourseValues() {
+  let course_name = document.getElementById("cn").value;
+  if (!course_name) {
+    alert("Please enter Course Name.");
+    return;
+  }
+  let createCourse = await fetch(`http://localhost:5050/insert-course`, {
+    method: "POST",
+
+    body: JSON.stringify({
+      CourseName: course_name,
+    }),
+  });
+  let response = await createCourse.json();
+  if (!createCourse.ok) {
+    response_for_creation.innerHTML = response + "!!";
+  } else {
+    console.log(response);
+    let response_for_creation = document.getElementById(
+      "response_for_creation"
+    );
+    response_for_creation.innerHTML = "Successffully Created";
+  }
+}
