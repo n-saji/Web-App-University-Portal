2f9c11106e4ef96f29946d473a563e33a39326c1
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2023-01-05 15:47:48 +0530

    making go routines synchronuc

--- service/cronMigrations.go
+++ service/cronMigrations.go
@@ -2,17 +2,25 @@ package service
 
 import (
 	"log"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 func (s *Service) RunDailyMigrations() {
+	//making go routines synchronous 
+	wg.Add(2)
+	ch := make(chan int)
 	log.Println("running migrations")
-	go s.CheckOutDatedTokensSetFalse()
-	go s.DeleteNotValidTokens()
+	go s.CheckOutDatedTokensSetFalse(ch)
+	go s.DeleteNotValidTokens(ch)
+	wg.Wait()
 }
 
-func (s *Service) CheckOutDatedTokensSetFalse() {
+func (s *Service) CheckOutDatedTokensSetFalse(ch chan int) {
 
+	ch <- 1
 	all_tokens, err := s.daos.GetAllTokens()
 	if err != nil {
 		log.Panic(err)
@@ -25,11 +33,15 @@ func (s *Service) CheckOutDatedTokensSetFalse() {
 		}
 
 	}
+	defer wg.Done()
 }
 
-func (s *Service) DeleteNotValidTokens() {
+func (s *Service) DeleteNotValidTokens(ch chan int) {
+
+	<-ch
 	err := s.daos.RunMigrationsForRemovingOutDatedTokens()
 	if err != nil {
 		log.Panic(err)
 	}
+	defer wg.Done()
 }
