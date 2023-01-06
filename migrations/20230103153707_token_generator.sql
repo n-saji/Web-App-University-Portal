d53209fdd985ad9f5a2e10799bc176f9b76b48a2
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2023-01-06 13:43:01 +0530

    changed timestamp

--- migrations/20230103153707_token_generator.sql
+++ migrations/20230103153707_token_generator.sql
@@ -2,8 +2,8 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS token_generators (
     token uuid not null,
-    valid_from timestamp null,
-    valid_till timestamp null,
+    valid_from numeric null,
+    valid_till numeric null,
     is_valid boolean null,
     CONSTRAINT token_generators_pkey PRIMARY KEY (token)
 );
