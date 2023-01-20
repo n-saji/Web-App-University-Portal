cf08f933397a5863f185270f3c2008a0eae24291
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2023-01-20 13:41:37 +0530

    configered db url

---
+++ utils/validate_token.go
@@ -0,0 +1,18 @@
+package utils
+
+func CheckTokenWithCookie(token string) error {
+
+	//s := service.New(daos.AdminstrationCloud)
+	// token_id, err4 := uuid.Parse(token)
+	// if err4 != nil {
+	// 	return fmt.Errorf("error parsing uuid")
+	// }
+	//status, err1 := s.CheckTokenValidity(token_id)
+	//if err1 != nil {
+	//	return err1
+	//}
+	//if !status {
+	//	return fmt.Errorf("token expired")
+	//}
+	return nil
+}
