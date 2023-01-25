8ea47d0d12f635525defa22b641765d93cd34ada
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2023-01-25 12:33:35 +0530

    TODO

--- frontEnd/allCourse.js
+++ frontEnd/allCourse.js
@@ -1,26 +1,26 @@
 let dropdown = document.getElementById("cn_drop_down");
-dropdown.length = 0;
-
 let defaultOption = document.createElement("option");
-defaultOption.text = "Choose Course";
 let cookie_token = getCookie("token");
+const url = "http://localhost:5050/retrieve-all-courses";
+dropdown.length = 0;
+defaultOption.text = "Choose Course";
 dropdown.add(defaultOption);
 dropdown.selectedIndex = 0;
-
-const url = "http://localhost:5050/retrieve-all-courses";
-
 fetch(url, {
   headers: { token: cookie_token },
 })
+
   .then(function (response) {
     if (response.status !== 200) {
       console.warn(
         "Looks like there was a problem. Status Code: " + response.status
       );
+      response.json().then(function (data) {
+          alert(data)
+          setTimeout(window.location.replace("index.html"), 2000);
+        });
       return;
     }
-
-    // Examine the text in the response
     response.json().then(function (data) {
       let option;
 
@@ -35,13 +35,13 @@ fetch(url, {
     console.error("Fetch Error -", err);
   });
 
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
+  return null;
+}
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -44,4 +44,5 @@ create new service for retrieve individual details
 delete handler not working frontend
 fe all pages check if working - cookie issue
 need to figure out how to update course
+make buttons as icon using heroicons
 */
