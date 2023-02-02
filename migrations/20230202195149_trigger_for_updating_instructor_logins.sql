8e0e7f38cfa19c7c61f3186eab9543cf2396fb7f
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2023-02-02 19:59:09 +0530

    added new triggers

---
+++ migrations/20230202195149_trigger_for_updating_instructor_logins.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION delete_instructor_login()
+RETURNS TRIGGER AS $my_table$
+BEGIN
+   DELETE FROM public.instructor_logins
+    WHERE instructor_id not in (SELECT id from  public.instructor_details);
+RETURN NEW;
+END;
+$my_table$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER delete_instructor_login_trigger
+after
+delete ON instructor_details FOR EACH ROW EXECUTE PROCEDURE delete_instructor_login();
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TRIGGER IF EXISTS delete_instructor_login_trigger on instructor_details;
+-- +goose StatementEnd
