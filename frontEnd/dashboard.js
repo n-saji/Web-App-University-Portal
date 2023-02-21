2c05c9534b9c079ecbb1f8b9a5a98f0db3c1ec87
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2023-02-21 17:20:15 +0530

    added ui changes w.r.t token status api

--- frontEnd/dashboard.js
+++ frontEnd/dashboard.js
@@ -69,3 +69,26 @@ function getCookie(name) {
   return null;
 }
 getInstructorDetails();
+setInterval(checkTokenValidity, 300000);
+
+async function checkTokenValidity() {
+  let api_error;
+  let cookie_token = getCookie("token");
+  let api_response = await fetch(`http://localhost:5050/check-token-status`, {
+    method: "GET",
+    headers: { Token: cookie_token },
+  }).catch((err) => {
+    api_error = err;
+  });
+  if (api_error == "TypeError: Failed to fetch") {
+    alert("Internal Server Error Please Login Again");
+    window.location.replace("index.html");
+    return "";
+  }
+  let response = await api_response.json();
+  if (response == "token expired! Generate new token") {
+    alert("Timed-out re login");
+    window.location.replace("index.html");
+    return;
+  }
+}
