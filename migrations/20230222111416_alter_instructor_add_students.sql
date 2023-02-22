c157aa1529d9587456ada50a67a9ab6f1bfc47fc
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2023-02-22 13:00:47 +0530

    changed column name in table

--- migrations/20230222111416_alter_instructor_add_students.sql
+++ migrations/20230222111416_alter_instructor_add_students.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 Alter table if exists instructor_details 
-add column if not exists students_list jsonb default '{}';
+add column if not exists info jsonb default '{}';
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 Alter table if exists instructor_details 
-drop column if exists students_list;
+drop column if exists info;
 -- +goose StatementEnd
