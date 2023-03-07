f9bf689a1955a7a17af053d5bbeb3b6a28918641
Nikhil-Wiz <103165218+Nikhil-Wiz@users.noreply.github.com>
2023-03-07 17:00:30 +0530

    migrations bug fix

--- migrations/20230307162832_table_account.sql
+++ migrations/20230307162832_table_account.sql
@@ -1,15 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS account(
+CREATE TABLE IF NOT EXISTS accounts(
     id uuid not null,
     name varchar null,
     info jsonb,
-    CONSTRAINT account_pkey PRIMARY KEY (id)
+    CONSTRAINT accounts_pkey PRIMARY KEY (id)
 )
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE IF EXISTS account DROP CONSTRAINT IF EXISTS account_pkey;
-DROP TABLE IF EXISTS account;
+ALTER TABLE IF EXISTS accounts DROP CONSTRAINT IF EXISTS accounts_pkey;
+DROP TABLE IF EXISTS accounts;
 -- +goose StatementEnd
