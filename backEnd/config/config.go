3b21e522e7d51f73f7b681ae6886b4b65a058f2a
Nikhil Saji <nikhilsaji200@gmail.com>
2025-02-03 17:45:04 -0500

    added secrets

--- backEnd/config/config.go
+++ backEnd/config/config.go
@@ -2,8 +2,11 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -17,12 +20,11 @@ var (
 )
 
 func Init() {
-	os.Setenv("POSTGRES_USER", "postgres")
-	os.Setenv("POSTGRES_DB_NAME", "postgres")
-	os.Setenv("POSTGRES_PASSWORD", "password")
-	os.Setenv("POSTGRES_PORT", "5432")
-	os.Setenv("POSTGRES_HOST", "db")
-	os.Setenv("PORT", ":5050")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file - ",err)
+	}
 
 	Postgres_User = os.Getenv("POSTGRES_USER")
 	Postgres_Db_Name = os.Getenv("POSTGRES_DB_NAME")
@@ -31,6 +33,6 @@ func Init() {
 	Postgres_Host = os.Getenv("POSTGRES_HOST")
 	Port = os.Getenv("PORT")
 
-	os.Setenv("db_url", fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", Postgres_Host, Postgres_User, Postgres_Password, Postgres_Db_Name, Postgres_Port))
+	os.Setenv("db_url", fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=require", Postgres_Host, Postgres_User, Postgres_Password, Postgres_Db_Name, Postgres_Port))
 	DB_URL = os.Getenv("db_url")
 }
--- backEnd/config/daos.go
+++ backEnd/config/daos.go
@@ -10,6 +10,7 @@ import (
 func DBInit() *gorm.DB {
 
 	db, err := gorm.Open(postgres.Open(DB_URL), &gorm.Config{})
+	log.Println("Connected to database",DB_URL)
 	if err != nil {
 		log.Println("Found err while connecting to database", err)
 	}
