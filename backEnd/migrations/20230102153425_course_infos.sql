e39b721fd618f5909136d63d8475caefce0e83d3
Nikhil Saji <nikhilsaji200@gmail.com>
2025-02-06 14:39:08 -0500

    Added script to insert data

--- backEnd/migrations/20230102153425_course_infos.sql
+++ backEnd/migrations/20230102153425_course_infos.sql
@@ -9,6 +9,19 @@ CREATE TABLE IF NOT EXISTS course_infos (
 INSERT INTO public.course_infos
 (id, course_name)
 VALUES('285aa383-53b5-4e26-bf3a-c76b3245c617', 'No Course');
+
+INSERT INTO course_infos (id, course_name) VALUES
+('550e8400-e29b-41d4-a716-446655440000', 'Introduction to Computer Science'),
+('123e4567-e89b-12d3-a456-426614174001', 'Data Structures and Algorithms'),
+('987f6543-b21c-34d2-c789-123456789abc', 'Database Management Systems'),
+('456e7890-c12d-45e6-b789-987654321def', 'Operating Systems'),
+('321f6547-d34e-56f7-e890-234567890ghi', 'Artificial Intelligence'),
+('789a1234-e45f-67g8-f901-345678901jkl', 'Machine Learning'),
+('654b3210-f56g-78h9-g012-456789012mno', 'Software Engineering'),
+('234c5678-g78h-90i1-h123-567890123pqr', 'Computer Networks'),
+('890d1234-h90i-12j3-i234-678901234stu', 'Web Development'),
+('567e8901-i12j-34k5-j345-789012345vwx', 'Cloud Computing');
+
 -- +goose StatementEnd
 
 -- +goose Down
