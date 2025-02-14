f8cc27214782092b61168cac915225fb2d352b89
Nikhil Saji <nikhilsaji200@gmail.com>
2025-02-13 19:29:42 -0500

    Bug in trigger

--- backEnd/migrations/20230307170900_trigger_account.sql
+++ backEnd/migrations/20230307170900_trigger_account.sql
@@ -13,7 +13,10 @@ CREATE OR REPLACE FUNCTION delete_account_ids()
 RETURNS TRIGGER AS $my_table$
 BEGIN
    DELETE FROM public.accounts
-    WHERE id::text not in (SELECT si.id from  public.student_infos si UNION SELECT ids.id from  public.instructor_details ids);
+    WHERE id::text not in 
+        (SELECT si.id::text from  public.student_infos si 
+        UNION 
+        SELECT ids.id::text from  public.instructor_details ids);
 RETURN NEW;
 END;
 $my_table$ LANGUAGE plpgsql;
